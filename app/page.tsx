784114c1855302e3ec325c3260dcb5a7a95cfd9f
Mohamed Ilaiwi <burkmoh@gmail.com>
2025-08-03 15:18:14 -0400

    failed: setting up blocknote editor

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ import React from 'react';
 import '../frontend/globals.css'
 import App from '../frontend/src/components/Layout/App';
 
+// CSS files for blocknote
+import '@blocknote/core/fonts/inter.css'
+import '@blocknote/mantine/style.css'
+
 export default function HomePage() {
     return (
         <html>
--- frontend/src/components/Layout/EditorManager/EditorManager.tsx
+++ frontend/src/components/Layout/EditorManager/EditorManager.tsx
@@ -1,20 +1,36 @@
 // frontend/src/components/Layout/EditorManager
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useFileSystem } from '@/src/contexts/FileSystemContext'
+import { useCreateBlockNote } from '@blocknote/react'
+import { BlockNoteView } from '@blocknote/mantine'
+import { Block } from '@blocknote/core'
+
+import './style.css'
 
 interface EditorManagerProps {
 
 }
 
 const EditorManager: React.FC<EditorManagerProps> = ({}) => {
+    const [blocks, setBlocks] = useState<Block[]>([])
+
     const { currentFilePath, currentContent } = useFileSystem()
 
+    const editor = useCreateBlockNote()
+
     useEffect(() => {
-        console.log(`file path has changed!`)
     }, [currentFilePath])
 
+
     return (
-        <h1>{currentContent}</h1>
+        <div className='w-full h-full'>
+            <BlockNoteView 
+                editor={editor} 
+                onChange={() => {
+                    setBlocks(editor.document)
+                }}
+            />
+        </div>
     )
 }
 
